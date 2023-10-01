9bb4fe4d41e816abd6c0e26ee18e9dd53d322d1d
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-01 15:41:05 -0600

    Add files via upload

---
+++ Unidad 2/Programa8.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Programa8
+{
+    class Hexagono
+    {
+        static void Main(string[] args)
+        {
+            int Lado, Perímetro, Apótema, Área;
+            Console.WriteLine("Introducir valor de un lado del hexagono");
+            Lado = Convert.ToInt32(Console.ReadLine());
+            Perímetro = Lado * 6;
+            Console.WriteLine("Introducir valor del Apótema");
+            Apótema = Convert.ToInt32(Console.ReadLine());
+            Área = (Perímetro * Apótema) / 2;
+            Console.WriteLine("El Área es igual a :" + Área);          
+
+        }
+    }
+}
