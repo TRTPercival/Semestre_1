fa1f1091198145390e24f388ceba799904433397
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-01 15:22:06 -0600

    Add files via upload

---
+++ Unidad 2/Programa7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programa7
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+            int x, y, suma, resta, multiplicación, división;
+            Console.WriteLine("Calculadora Básica");
+            Console.WriteLine("Ingrese el primer número:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número:");
+            y = Convert.ToInt32(Console.ReadLine());
+            suma = x + y;
+            Console.WriteLine("La suma es igual a: " + suma);
+            resta = x - y;
+            Console.WriteLine("La resta es igual a: " + resta);
+            multiplicación = x * y;
+            Console.WriteLine("La multiplicación es igual a: " + multiplicación);
+            división = x / y;
+            Console.WriteLine("La división es igual a: " + división);
+        }
+    }
+}
