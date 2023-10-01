dce16f3fac15f37b5f7232164d3d687a4bf62ff3
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-01 14:33:36 -0600

    Add files via upload

---
+++ Unidad 2/P1.cs
@@ -0,0 +1,16 @@
+﻿using System; //Paso 1
+
+namespace P_1 //Paso 1
+{
+    internal class Program
+    {
+        static void Main(string[] args) //Paso 1
+        {
+            //DECLARAR VARIABLES
+            string nombre = "José Cárdenas"; //Paso 2 
+            //Imprimir nombre referenciado a una variable
+            Console.WriteLine(nombre); //Paso 3 
+            //Cada línea debe finalizar con ; punto y coma
+        }//Paso 4 
+    }//Paso 4
+}//Paso4
