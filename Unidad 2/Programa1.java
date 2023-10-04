6b6d726c8a2de1b4c39229a9f2d71528f4767d5f
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-03 19:56:01 -0600

    Add files via upload

---
+++ Unidad 2/Programa1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa1;
+import java.lang.*;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa1 {
+
+    public static void main(String[] args) {
+        String nombre="José Cárdenas";
+        
+        System.out.println(nombre);
+    }
+}
