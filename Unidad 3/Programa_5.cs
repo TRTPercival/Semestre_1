9352ec60988663f40825a8e85553d5ba56b02b45
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-17 19:57:04 -0600

    Add files via upload

---
+++ Unidad 3/Programa_5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programa_5;
+    internal class Program
+    {
+    public static void Main(String[] args)
+    {
+        
+        double p1, p2, p3, total;
+        Console.WriteLine("Escribe precio 1");
+        p1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Escribe precio 2");
+        p2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Escribe precio 3");
+        p3 = Convert.ToInt32(Console.ReadLine());
+        total = p1 + p2 + p3;
+        if (total >= 1500)
+        {
+            total = total - (total * 0.30);
+            Console.WriteLine("El total (30%desc): " + total);
+
+        }
+        else if (total < 1500 && total >= 1000)
+        {
+            total = total - (total * 0.20);
+            Console.WriteLine("El total (20%desc): " + total);
+        }
+        else if (total < 1000 && total >= 700)
+        {
+            total = total - (total * 0.10);
+            Console.WriteLine("El total (10%desc): " + total);
+        }
+        else
+            Console.WriteLine("El total (sin desc): " + total);
+    }
+}
