7a1afa93d4c8a813bf43e181377b4170eea4caad
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-23 19:54:55 -0600

    Add files via upload

---
+++ Unidad 3/Programa11.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa11;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa11 {
+
+    public static void main(String[] args) {
+        int c;
+        for (c=1; c<=100; c++){
+        System.out.println(c);
+        }
+        for (int i=100; i>=1; i--){
+            System.out.println(i);
+
+        }
+    }
+}
