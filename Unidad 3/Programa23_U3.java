94f9cbc16821d68e100b0bd0c7cdd0a44ed393ee
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-10-31 19:49:50 -0600

    Add files via upload

---
+++ Unidad 3/Programa23_U3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa23_u3;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa23_U3 {
+
+    public static void main(String[] args) {
+        int numero=0;
+        do{
+            System.out.println(numero);
+            numero++;
+        } while(numero<=10);
+                
+    }
+}
