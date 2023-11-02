548745ecfb3e25698a4cf3f994a6d76fca9d6b39
TRTPercival <146492121+TRTPercival@users.noreply.github.com>
2023-11-01 19:52:31 -0600

    Add files via upload

---
+++ Unidad 3/Programa26__U3.cs
@@ -0,0 +1,41 @@
+﻿
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa26_U3
+{
+
+    public static void Main(String[] args)
+    {
+        int c = 1;
+        double salario = 1000.0;
+        double suma = 0.0;
+
+        while (c <= 6)
+        {
+
+            salario = salario + 100;
+
+
+            Console.WriteLine("Tu salario mensual del año " + c + " es: " + salario);
+
+
+            double sueldoAnual = salario * 12;
+
+
+            Console.WriteLine("Tu salario del año " + c + " es: " + sueldoAnual);
+
+
+            suma = suma + sueldoAnual;
+
+
+            c++;
+        }
+
+
+        Console.WriteLine("Tu sueldo acumulado por 6 años es: " + suma);
+
+
+    }
+}
